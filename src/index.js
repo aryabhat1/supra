cfb11764f30803d6c7ccb9803743e0c45edae9db
Sushant Prusty <sprusty@pop-os.localdomain>
2022-12-12 13:28:15 +0530

    Let's deploy supra client

--- src/index.js
+++ src/index.js
@@ -6,17 +6,20 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { store } from "./app/store";
 import { Provider } from "react-redux";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <Router>
-        {/* <App /> */}
-        <Routes>
-          <Route path="/*" element={<App />}></Route>
-        </Routes>
-      </Router>
-    </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <Router>
+                {/* <App /> */}
+                <Routes>
+                    <Route path="/*" element={<App />}></Route>
+                </Routes>
+            </Router>
+        </Provider>
+    </React.StrictMode>
 );
